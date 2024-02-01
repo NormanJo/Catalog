1c041b880f96f5072fbd431cab17237296e5e831
NormanJo <77800615+NormanJo@users.noreply.github.com>
2024-02-01 12:06:31 +0600

    Add files via upload

---
+++ js/mouse.js
@@ -0,0 +1,17 @@
+$( ".color-black" ).click(function() {
+    $(this).css( "color", "white" );
+    $(".color-white").css( "color", "white" );
+    $(".blackwhite").html("Black");
+    src="https://media.ldlc.com/r1600/ld/products/00/05/00/34/LD0005003477_2.jpg";
+    $(".headset").attr("src", src);
+  });
+  
+  $( ".color-white" ).click(function() {
+    $(this).css( "color", "black" );
+    $(".color-black").css( "color", "black" );
+    $(".blackwhite").html("White");
+    src="https://media.ldlc.com/r1600/ld/products/00/05/00/35/LD0005003502_2.jpg";
+    $(".headset").attr("src", src);
+  });
+  
+  
