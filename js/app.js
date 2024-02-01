f055e1b8a4f94cda4b305af85c0f4bfca31fe6dd
NormanJo <77800615+NormanJo@users.noreply.github.com>
2024-02-02 01:51:43 +0600

    app.js

--- js/app.js
+++ js/app.js
@@ -23,139 +23,161 @@ let hed2 = document.getElementById("hed2");
 let gam1 = document.getElementById("gam1");
 let gam2 = document.getElementById("gam2");
 
+if (ph1) {
+	ph1.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар телефон 1!");
+			item = "1";
+			tg.MainButton.show();
+		}
+	});
+}
 
-ph1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар телефон 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
+if (ph2) {
+	ph2.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар телефон 2!");
+			item = "2";
+			tg.MainButton.show();
+		}
+	});
+}
 
-ph2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар телефон 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
+if (mou1) {
+	mou1.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар мышка 1!");
+			item = "3";
+			tg.MainButton.show();
+		}
+	});
+}
 
-mou1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар мышка 1!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
+if (mou2) {
+	mou2.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар мышка 2!");
+			item = "4";
+			tg.MainButton.show();
+		}
+	});
+}
 
-mou2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар мышка 2!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
+if (mic1) {
+	mic1.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар микрофон 1!");
+			item = "5";
+			tg.MainButton.show();
+		}
+	});
+}
 
-mic1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар микрофон 1!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
+if (mic2) {
+	mic2.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар микрофон 2!");
+			item = "6";
+			tg.MainButton.show();
+		}
+	});
+}
 
-mic2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар микрофон 2!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
+if (key1) {
+	key1.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар клавиатура 1!");
+			item = "7";
+			tg.MainButton.show();
+		}
+	});
+}
 
-key1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар клавиатура 1!");
-		item = "7";
-		tg.MainButton.show();
-	}
-});
+if (key2) {
+	key2.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар клавиатура 2!");
+			item = "8";
+			tg.MainButton.show();
+		}
+	});
+}
 
-key2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар клавиатура 2!");
-		item = "8";
-		tg.MainButton.show();
-	}
-});
+if (hed1) {
+	hed1.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар наушники 1!");
+			item = "9";
+			tg.MainButton.show();
+		}
+	});
+}
 
-hed1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар наушники 1!");
-		item = "9";
-		tg.MainButton.show();
-	}
-});
+if (hed2) {
+	hed2.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар наушники 2!");
+			item = "10";
+			tg.MainButton.show();
+		}
+	});
+}
 
-hed2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар наушники 2!");
-		item = "10";
-		tg.MainButton.show();
-	}
-});
+if (gam1) {	
+	gam1.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар геймпад 1!");
+			item = "11";
+			tg.MainButton.show();
+		}
+	});
+}
 
-gam1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар геймпад 1!");
-		item = "11";
-		tg.MainButton.show();
-	}
-});
-
-gam2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар геймпад 2!");
-		item = "12";
-		tg.MainButton.show();
-	}
-});
-
+if (gam2) {
+	gam2.addEventListener("click", function(){
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide();
+		}
+		else {
+			tg.MainButton.setText("Вы выбрали товар геймпад 2!");
+			item = "12";
+			tg.MainButton.show();
+		}
+	});
+}
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
