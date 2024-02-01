8c0d5dea6087a405c5d54e3f32ef91e94ea99e52
NormanJo <77800615+NormanJo@users.noreply.github.com>
2024-02-02 02:08:28 +0600

    app.js

--- js/app.js
+++ js/app.js
@@ -184,5 +184,13 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	window.close();
 });
 
+let usercard = document.getElementById("usercard");
 
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
+	
 };
